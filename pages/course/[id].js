39560934f4d9c183ecf612aa1788ec3a85a8faa4
Noortaz Ahmed <nahmed117m@gmail.com>
2025-06-10 12:53:17 +0600

    link bt

--- pages/course/[id].js
+++ pages/course/[id].js
@@ -219,7 +219,7 @@ export default function CourseDetail() {
                     d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                   />
                 </svg>
-                Open in Google Drive
+                Open Course
               </motion.button>
             </div>
           </div>
